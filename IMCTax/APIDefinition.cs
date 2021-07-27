b66eba3a137ad6ea9abc91ceb8b0461b15c4a98f
programmerjeff-imctaxgithub <88055124+programmerjeff-imctaxgithub@users.noreply.github.com>
2021-07-27 14:36:52 -0400

    Add files via upload

---
+++ IMCTax/APIDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IMCTax
+{
+    public class APIDefinition
+    {
+        public string Name { get; set; }
+        public string APIKey { get; set; }
+        public string APIURI { get; set; }
+
+        public Dictionary<string, string> Actions { get; set; }
+    }
+}
